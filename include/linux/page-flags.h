bd801166125b90506684c42f2cd8f6987bd714ee
arter97 <qkrwngud825@gmail.com>
2013-07-24 13:00:20 +0900

    backport SLUB from Linux 3.3 (thanks to faux123)
    Conflicts:
    	include/linux/page-flags.h
    
    Change-Id: I185253455ab5cb45014be09ee0058d056ceb0d47
    
    Conflicts:
    	include/linux/page-flags.h
    
    Conflicts:
    	include/linux/page-flags.h

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -137,9 +137,6 @@ enum pageflags {
 
 	/* SLOB */
 	PG_slob_free = PG_private,
-
-	/* SLUB */
-	PG_slub_frozen = PG_active,
 };
 
 #ifndef __GENERATING_BOUNDS_H
@@ -225,8 +222,9 @@ PAGEFLAG(SwapBacked, swapbacked) __CLEARPAGEFLAG(SwapBacked, swapbacked)
 
 __PAGEFLAG(SlobFree, slob_free)
 
-__PAGEFLAG(SlubFrozen, slub_frozen)
-
+#ifdef CONFIG_CLEANCACHE
+PAGEFLAG(WasActive, was_active)
+#endif
 /*
  * Private page markings that may be used by the filesystem that owns the page
  * for its own purposes.
