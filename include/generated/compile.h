77164aac13fe4a8cb498f79a0cc15cf61bf69e0b
k2wl <k2wl@github.com>
2013-11-26 07:40:43 +0530

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Nov 25 20:42:08 IST 2013"
+#define LINUX_COMPILE_BY "android"
+#define LINUX_COMPILE_HOST "android"
+#define LINUX_COMPILER "gcc version 4.7.4 20131111 (prerelease) (crosstool-NG hg+default-388793b84c11 - Linaro GCC 2013.11) "
