06a24ae138587784fdd52cf6080f3cc7f6b3b28c
k2wl <k2wl@github.com>
2013-12-13 09:22:23 +0530

    updated to 3.0.34

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -2,7 +2,7 @@
 #include <linux/broadcom/vc_debug_sym.h>
 
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "android" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "8a18c607895269b8ec53fc1c106e0e24e7cb3ab2 (tainted)" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "375094bbc88c241d7c4ec54e884482a4eeae81d7 (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
