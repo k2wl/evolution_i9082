426a606db6fdcf87ff1914b01c687addcc938ce2
k2wl <k2wl@github.com>
2013-12-15 09:20:31 +0530

    added arm topology support with optimizations

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -1,8 +1,8 @@
 #include "vchiq_build_info.h"
 #include <linux/broadcom/vc_debug_sym.h>
 
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "SEV-DEV-SOLUTION" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "unknown" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "android" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "c3599ae945763cd61f94d2cb02b4017115605a86 (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
