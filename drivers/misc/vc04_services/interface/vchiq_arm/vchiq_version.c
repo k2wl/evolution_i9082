0a2476bf7110afce36b3709b46c1ae2a9ffa709f
k2wl <k2wl@github.com>
2013-12-16 15:37:30 +0530

    Add governers with few conflics

--- drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
+++ drivers/misc/vc04_services/interface/vchiq_arm/vchiq_version.c
@@ -2,7 +2,7 @@
 #include <linux/broadcom/vc_debug_sym.h>
 
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_hostname, "android" );
-VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "79674b934960561d2961bbad29353fd4bdc36f38 (tainted)" );
+VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_version, "62bd2bb4b2337ebb1db19bf9aad3a9736c77f5e2 (tainted)" );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_time,    __TIME__ );
 VC_DEBUG_DECLARE_STRING_VAR( vchiq_build_date,    __DATE__ );
 
