406d2355db8e36a62ffd3541e6c47fb4059939a7
k2wl <k2wl@github.com>
2013-12-16 19:13:54 +0530

    experimental Touch-to-Wake implementation part 1

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -25,6 +25,13 @@
 #include <linux/gpio_keys.h>
 #include <linux/workqueue.h>
 #include <linux/gpio.h>
+#include <linux/irqdesc.h>
+
+#ifdef CONFIG_TOUCH_WAKE
+#include <linux/touch_wake.h>
+#endif
+
+extern struct class *sec_class;
 
 struct gpio_button_data {
 	struct gpio_keys_button *button;
@@ -561,6 +568,10 @@ static int __devinit gpio_keys_probe(struct platform_device *pdev)
 			wakeup = 1;
 
 		input_set_capability(input, type, button->code);
+
+		#ifdef CONFIG_TOUCH_WAKE
+			  set_powerkeydev(input);
+		#endif
 	}
 
 	error = sysfs_create_group(&pdev->dev.kobj, &gpio_keys_attr_group);
