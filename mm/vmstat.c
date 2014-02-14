ba01e6bbe7581da4d4621c09ec5776c9e34c58c2
k2wl <k2wl@github.com>
2014-02-14 21:09:18 +0530

    Add UKSM 0.1.2

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -741,6 +741,9 @@ const char * const vmstat_text[] = {
 	"contigalloc",
 #endif
 	"nr_anon_transparent_hugepages",
+#ifdef CONFIG_UKSM
+        "nr_uksm_zero_pages",
+#endif
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
 
