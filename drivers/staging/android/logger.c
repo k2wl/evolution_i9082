9a7b45d8c3c19e0fd8b77930f5bc750ecc37beed
k2wl <k2wl@github.com>
2014-04-26 18:42:32 +0530

    Logger: Add an enable toggle for entry storage; TNX AndreiLux and Dorimanx
    You can disable logging by echo-ing 0 in /sys/module/logger/parameters/log_enabled
    By default the logging is enabled.
    
    Signed-off-by: k2wl <k2wl@github.com>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -33,6 +33,9 @@
 #include <mach/sec_debug.h>
 #endif
 
+static unsigned int log_enabled = 1;
+module_param(log_enabled, uint, S_IWUSR | S_IRUGO);
+
 /*
  * struct logger_log - represents a specific log, such as 'main' or 'radio'
  *
@@ -442,6 +445,9 @@ ssize_t logger_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct logger_entry header;
 	struct timespec now;
 	ssize_t ret = 0;
+
+    if (!log_enabled)
+        return 0;
 
 	now = current_kernel_time();
 
