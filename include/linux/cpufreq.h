1e55d407e2dcb1b3702a5d61ed29f79bda5a5ec8
k2wl <k2wl@github.com>
2014-04-27 16:28:45 +0530

    Added ondemandX Governor
    Signed-off-by: k2wl <k2wl@github.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -382,6 +382,10 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
 extern struct cpufreq_governor cpufreq_gov_adaptive;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_adaptive)   
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_ondemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandx)
+
 #endif
 
 
