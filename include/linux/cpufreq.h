9a8cea1631deb40a273a35da57a9f9f68f91dda2
k2wl <k2wl@github.com>
2014-04-29 00:11:40 +0530

    Added InteractiveX Governor with hotplug support by imoseyon
    Source: https://github.com/imoseyon/leanKernel-galaxy-nexus/commit/46fafb56f76f669efb9b618e8efaf1ad612bb97b
    Signed-off-by: k2wl <k2wl@github.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,6 +388,8 @@ extern struct cpufreq_governor cpufreq_gov_ondemandx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
 #endif
 
 
